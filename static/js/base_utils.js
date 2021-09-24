8aa69ad8531b5963d1f235dcd5b77c296f3d12ee
Abdullah Malallah <91322221+AbdMala@users.noreply.github.com>
2021-09-24 14:15:24 +0200

    Delete static/js directory

--- static/js/base_utils.js
+++
@@ -1,8 +0,0 @@
-function full_screen(id) {
-    let element = $(`#${id}`)
-    element.height($("#SWAG-footer").offset().top - element.offset().top);
-
-    $(window).resize(function () {
-        element.height($("#SWAG-footer").offset().top - element.offset().top);
-    })
-}
